9572d0ecd331885651fa09c0668ac7f760117f57
Finborough <113507556+Finborough@users.noreply.github.com>
2026-04-19 11:58:04 +0300

    movement

---
+++ Assets/Scenes/PlayerMove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speed = 6.0F;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    public Camera camera;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
+    float rotationX = 0;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+void Update() {
+    CharacterController controller = GetComponent<CharacterController>();
+
+    Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+
+    move = transform.TransformDirection(move);
+    move *= speed;
+
+    controller.Move(move * Time.deltaTime);
+    }
+}
